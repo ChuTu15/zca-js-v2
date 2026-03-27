6ee7369c26ee29330c0ddbb7ba829a2c9cd728f5
Chu Quang Tú <tucq@ezcloud.vn>
2026-03-27 15:41:20 +0700

    first commit

---
+++ src/apis/getAllFriends.ts
@@ -0,0 +1,43 @@
+import { ZaloApiError } from "../Errors/ZaloApiError.js";
+import { apiFactory } from "../utils.js";
+
+import type { User } from "../models/index.js";
+
+export type GetAllFriendsResponse = User[];
+
+export const getAllFriendsFactory = apiFactory<GetAllFriendsResponse>()((api, ctx, utils) => {
+    const serviceURL = utils.makeURL(`${api.zpwServiceMap.profile[0]}/api/social/friend/getfriends`);
+
+    /**
+     * Get all friends
+     *
+     * @param count Page size (default: 20000)
+     * @param page Page number (default: 1)
+     *
+     * @throws {ZaloApiError}
+     */
+    return async function getAllFriends(count: number = 20000, page: number = 1) {
+        const params = {
+            incInvalid: 1,
+            page,
+            count,
+            avatar_size: 120,
+            actiontime: 0,
+            imei: ctx.imei,
+        };
+
+        const encryptedParams = utils.encodeAES(JSON.stringify(params));
+        if (!encryptedParams) throw new ZaloApiError("Failed to encrypt message");
+
+        const response = await utils.request(
+            utils.makeURL(serviceURL, {
+                params: encryptedParams,
+            }),
+            {
+                method: "GET",
+            },
+        );
+
+        return utils.resolve(response);
+    };
+});
